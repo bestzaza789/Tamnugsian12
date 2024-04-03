175b7efe3dac7f40b2a8753e5181c23436282e56
bestzaza789 <146432574+bestzaza789@users.noreply.github.com>
2024-04-03 11:13:06 +0700

    fix:position picture reward

--- src/app/reward/page.tsx
+++ src/app/reward/page.tsx
@@ -5,42 +5,25 @@ export default function News() {
   return (
     <div className="w-full flex flex-row justify-around items-center">
       <div className="flex flex-col justify-around items-center m-[20px] md:m-[50px] p-[20px] py-[20px]  animate-fade-right animate-once animate-ease-in-out animate-normal animate-fill-forwards">
-        <div className="flex">
-          <Image
-            src="https://lh3.googleusercontent.com/d/1p8kqHQf6BSP6V5L7n5EEw1c7TyctfUk6"
-            width="1500"
-            height="1000"
-            alt="reward"
-            className="md:w-[500px] w-2/3 my-[20px] mx-1"
-          />
-          <Image
-            src="https://lh3.googleusercontent.com/d/1WSEGfDaAyjXl8hI5gDqO1ZRbhr-O4NfI"
-            width="1500"
-            height="800"
-            alt="reward"
-            className="md:w-[300px] w-1/3 my-[20px] mx-1"
-          />
-        </div>
+        <Image
+          src="https://lh3.googleusercontent.com/d/1p8kqHQf6BSP6V5L7n5EEw1c7TyctfUk6"
+          width="1500"
+          height="1000"
+          alt="reward"
+          className="md:w-[500px] w-[300px] my-[20px] mx-1"
+        />
 
         <h1 className=" text-[15px] my-[10px] md:text-[30px] md:my-[20px] text-center">
           รางวัลกินรีทอง ครั้งที่ 9 ปี พ.ศ. 2567
         </h1>
-        <div className="flex">
-          <Image
-            src="https://lh3.googleusercontent.com/d/1GPZanLZJuLNJpUVQCJyNzZjlgDvJdRo1"
-            width="1500"
-            height="800"
-            alt="reward"
-            className="md:w-[500px] w-2/3 my-[20px] mx-1"
-          />
-          <Image
-            src="https://lh3.googleusercontent.com/d/1ces8Ju2Sd6YfrltkrHHic3plTrVb5ltz"
-            width="1500"
-            height="800"
-            alt="reward"
-            className="md:w-[300px] w-1/3 my-[20px] mx-1"
-          />
-        </div>
+
+        <Image
+          src="https://lh3.googleusercontent.com/d/1GPZanLZJuLNJpUVQCJyNzZjlgDvJdRo1"
+          width="1500"
+          height="800"
+          alt="reward"
+          className="md:w-[500px] w-[300px] my-[20px] mx-1"
+        />
 
         <h1 className=" text-[15px] my-[10px] md:text-[30px] md:my-[20px] text-center">
           รางวัลกินรีทอง ครั้งที่ 8 ปี พ.ศ. 2566
@@ -56,6 +39,20 @@ export default function News() {
         <h1 className=" text-[15px] my-[10px] md:text-[30px] md:my-[20px] text-center">
           เกียรติรางวัล อ.หาริ เพชรลานน์ ผู้ก่อตั้ง ตำหนักเซียน 12 องค์เทพ
         </h1>
+        <Image
+          src="https://lh3.googleusercontent.com/d/1WSEGfDaAyjXl8hI5gDqO1ZRbhr-O4NfI"
+          width="1500"
+          height="800"
+          alt="reward"
+          className="md:w-[300px] w-[300px] my-[20px] mx-1"
+        />
+        <Image
+          src="https://lh3.googleusercontent.com/d/1ces8Ju2Sd6YfrltkrHHic3plTrVb5ltz"
+          width="1500"
+          height="800"
+          alt="reward"
+          className="md:w-[300px] w-[300px] my-[20px] mx-1"
+        />
       </div>
     </div>
   );
