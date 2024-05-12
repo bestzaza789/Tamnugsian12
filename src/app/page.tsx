8d9046a6cb146a9f5c73dd87fab136e6584e0eb6
bestzaza789 <146432574+bestzaza789@users.noreply.github.com>
2024-05-12 17:25:19 +0700

    fix laout homepage picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className=" w-full h-full z-0 flex flex-col items-center  ">
-      <div className="md:hidden m-5 p-2 bg-red-600 z-10 rounded-xl text-white text-center  w-3/4 ">
+      <div className="md:hidden mx-5 my-1 p-2 bg-red-600 z-10 rounded-xl text-white text-center  w-3/4 ">
         ศูนย์รวมผู้เชี่ยวชาญด้านโหราศาสตร์ตำหนักเซียน
       </div>
       <Image
@@ -15,7 +15,7 @@ export default function Home() {
         height="800"
       />
       <Image
-        className="w-full absolute top-[500px] md:relative -z-0 animate-fade animate-once animate-ease-in-out"
+        className="w-full absolute top-[300px] md:relative -z-0 animate-fade animate-once animate-ease-in-out"
         src="https://lh3.googleusercontent.com/d/1ORmbxt-5Gi7EWlh8pq6dak6YQ33ZookL"
         alt="history"
         width="1500"
